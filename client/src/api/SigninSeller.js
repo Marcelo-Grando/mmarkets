54898ddfa928d2aeba4f09783275d3b9aec2ed49
Marcelo-Grando <marcelogrando017@gmail.com>
2023-07-29 03:41:26 -0300

    code refactoring

--- client/src/api/SigninSeller.js
+++ client/src/api/SigninSeller.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export const signinSeller = async (seller) => 
-axios.post(`http://localhost:3000/api/signin`, seller)
+export const signinSeller = async (seller) =>
+  axios.post(`http://localhost:3000/api/signin`, seller);
